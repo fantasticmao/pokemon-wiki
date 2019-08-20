c19842fba50495e4928634847540dee98482b5e9
FantasticMao <maomao8017@gmail.com>
2019-08-20 12:15:26 +0800

    :relaxed:
    1. 支持查询宝可梦可学会的招

---
+++ src/main/java/cn/fantasticmao/pokemon/wiki/domain/PokemonDetailLearnSetByLevelingUp.java
@@ -0,0 +1,32 @@
+package cn.fantasticmao.pokemon.wiki.domain;
+
+import com.mundo.data.domain.AbstractDomain;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * PokemonDetailLearnSetByLevelingUp
+ *
+ * @author maomao
+ * @since 2019-08-20
+ */
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "pw_pokemon_detail_learn_set_by_leveling_up")
+public class PokemonDetailLearnSetByLevelingUp extends AbstractDomain<Integer> {
+    private int index;
+    private String level1;
+    private String level2;
+    private String move;
+    private String type;
+    private String category;
+    private String power;
+    private String accuracy;
+    private String pp;
+}
