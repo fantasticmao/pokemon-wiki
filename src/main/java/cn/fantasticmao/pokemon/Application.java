3a3d861890795800b2aa12e0475fc0f31f267ac0
FantasticMao <maomao8017@gmail.com>
2019-10-09 14:29:34 +0800

    :relaxed:
    1. 支持外部 Tomcat 容器部署

--- src/main/java/cn/fantasticmao/pokemon/Application.java
+++ src/main/java/cn/fantasticmao/pokemon/Application.java
@@ -2,9 +2,17 @@ package cn.fantasticmao.pokemon;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder)
+                .sources(Application.class)
+                .properties("spring.profiles.default=snapshot");
+    }
 
     /**
      * java -Xms100m -Xmx200m -XX:+HeapDumpOnOutOfMemoryError -XX:+UseConcMarkSweepGC -Dfile.encoding=UTF-8 -Dspring.profiles.active=master -jar pokemon-wiki-1.0.jar >> /var/log/pokemon-wiki/tomcat.log &
