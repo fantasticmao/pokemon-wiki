01241da491f66fc9a582663e36367be8bac600dc
fantasticmao <maomao8017@gmail.com>
2022-03-03 16:21:37 +0800

    :relaxed:
    1. 升级 JDK 11
    2. 添加单元测试

--- pokemon-wiki-web/src/test/java/cn/fantasticmao/pokemon/web/web/PokemonControllerTest.java
+++ pokemon-wiki-web/src/test/java/cn/fantasticmao/pokemon/web/web/PokemonControllerTest.java
@@ -1,7 +1,13 @@
 package cn.fantasticmao.pokemon.web.web;
 
+import cn.fantasticmao.mundo.core.util.JsonUtil;
+import cn.fantasticmao.mundo.web.support.JsonApi;
 import cn.fantasticmao.pokemon.web.SpringTest;
+import cn.fantasticmao.pokemon.web.bean.PokemonBean;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -9,6 +15,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.annotation.Resource;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * PokemonControllerTest
@@ -21,13 +28,64 @@ public class PokemonControllerTest extends SpringTest {
     private MockMvc mvc;
 
     @Test
-    public void listPokemonDetail() throws Exception {
+    public void listPokemonDetail_badRequest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/pokemon/detail"))
+            .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+            .andDo(result -> {
+                MockHttpServletResponse response = result.getResponse();
+                String bodyJson = response.getContentAsString(StandardCharsets.UTF_8);
+                JsonApi<List<PokemonBean>> jsonApi = JsonUtil.fromJson(bodyJson, new TypeReference<>() {
+                });
+
+                Assertions.assertFalse(jsonApi.isStatus());
+                Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), jsonApi.getCode());
+                Assertions.assertEquals(HttpStatus.BAD_REQUEST.getReasonPhrase(), jsonApi.getMessage());
+            });
+    }
+
+    @Test
+    public void listPokemonDetail_ok() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/pokemon/detail?index=1"))
             .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
             .andDo(result -> {
                 MockHttpServletResponse response = result.getResponse();
                 String bodyJson = response.getContentAsString(StandardCharsets.UTF_8);
-                System.out.println(bodyJson);
+                JsonApi<List<PokemonBean>> jsonApi = JsonUtil.fromJson(bodyJson, new TypeReference<>() {
+                });
+
+                Assertions.assertTrue(jsonApi.isStatus());
+                Assertions.assertEquals(HttpStatus.OK.value(), jsonApi.getCode());
+                Assertions.assertEquals(HttpStatus.OK.getReasonPhrase(), jsonApi.getMessage());
+
+                List<PokemonBean> list = jsonApi.getData();
+                Assertions.assertEquals(1, list.size());
+
+                PokemonBean pokemon = list.get(0);
+                Assertions.assertEquals(1, pokemon.getIndex());
+                Assertions.assertEquals("妙蛙种子", pokemon.getNameZh());
+            });
+    }
+
+    @Test
+    public void listPokemon_ok() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/pokemon/list?generation=1"))
+            .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+            .andDo(result -> {
+                MockHttpServletResponse response = result.getResponse();
+                String bodyJson = response.getContentAsString(StandardCharsets.UTF_8);
+                JsonApi<List<PokemonBean>> jsonApi = JsonUtil.fromJson(bodyJson, new TypeReference<>() {
+                });
+
+                Assertions.assertTrue(jsonApi.isStatus());
+                Assertions.assertEquals(HttpStatus.OK.value(), jsonApi.getCode());
+                Assertions.assertEquals(HttpStatus.OK.getReasonPhrase(), jsonApi.getMessage());
+
+                List<PokemonBean> list = jsonApi.getData();
+                Assertions.assertEquals(153, list.size());
+
+                PokemonBean pokemon = list.get(0);
+                Assertions.assertEquals(1, pokemon.getIndex());
+                Assertions.assertEquals("妙蛙种子", pokemon.getNameZh());
             });
     }
 }
